274f726347cbace17fb38f086297ec97c2fdfba0
Daniyal Khan <159725508+Danii96khan@users.noreply.github.com>
2024-05-10 01:33:52 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env Node
+import inquirer from "inquirer";
+
+let para = await inquirer.prompt([
+    {
+        name:"paragraph",
+        type:"string",
+        message:"Enter your paragraph"
+    }
+])
+let answer = para.paragraph.trim().split("")
+console.log(answer)
+console.log(answer.length)
